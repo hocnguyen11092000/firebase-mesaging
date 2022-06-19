fe9229173a7dea606e3eeb2cb1f7db4a9a1bcead
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-06-19 22:40:52 +0700

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAcV90sfGVFnRyPMSirOlOyVFi0et54pBw",
+  authDomain: "push-notification-7dfc4.firebaseapp.com",
+  projectId: "push-notification-7dfc4",
+  storageBucket: "push-notification-7dfc4.appspot.com",
+  messagingSenderId: "573174049824",
+  appId: "1:573174049824:web:56a3790a5414f9a23c2045",
+  measurementId: "G-6MK0REP3V6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
